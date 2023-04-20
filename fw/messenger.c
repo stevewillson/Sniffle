b556eed54fd429516719ddd129f55ffb2ec067cf
Steve Willson <swillson@mines.edu>
2023-04-20 14:31:41 -0600

    update messenger.c/h and sniffle.syscfg to use UART2

--- fw/messenger.c
+++ fw/messenger.c
@@ -4,119 +4,124 @@
  * Released as open source under GPLv3
  */
 
-#include "messenger.h"
-#include "base64.h"
-#include "ti_drivers_config.h"
 #include <stdbool.h>
 #include <ti/drivers/UART2.h>
+#include "ti_drivers_config.h"
+#include "messenger.h"
+#include "base64.h"
 
 UART2_Handle uart;
 
-int messenger_init() {
-  UART2_Params uartParams;
-  UART2_Params_init(&uartParams);
-  uartParams.baudRate = 2000000;
-  uartParams.readMode = UART2_Mode_BLOCKING;
-  uartParams.writeMode = UART2_Mode_BLOCKING;
-  uartParams.readReturnMode = UART2_ReadReturnMode_FULL;
-  uart = UART2_open(CONFIG_UART_0, &uartParams);
-  if (!uart)
-    return -1;
+int messenger_init()
+{
+    UART2_Params uartParams;
+    UART2_Params_init(&uartParams);
+    uartParams.baudRate = 2000000;
+    uartParams.readMode = UART2_Mode_BLOCKING;
+    uartParams.writeMode = UART2_Mode_BLOCKING;
+    uartParams.readReturnMode = UART2_ReadReturnMode_FULL;
+    uart = UART2_open(CONFIG_UART_0, &uartParams);
+    if (!uart)
+        return -1;
 
-  return 0;
+    return 0;
 }
 
 // keep doing small inefficient reads till we hit a CRLF
 // sorry, gotos appear to be the most elegant and correct approach here
-static void _recv_crlf() {
-  bool done = false;
-  uint8_t b = 0;
-  size_t bytes_read;
+static void _recv_crlf()
+{
+    bool done = false;
+    uint8_t b = 0;
 
-  while (!done) {
-    UART2_read(uart, &b, 1, &bytes_read);
-    while (b == '\r') {
-      UART2_read(uart, &b, 1, &bytes_read);
-      if (b == '\n')
-        done = true;
+    while (!done)
+    {
+        UART2_read(uart, &b, 1, NULL);
+        while (b == '\r')
+        {
+            UART2_read(uart, &b, 1, NULL);
+            if (b == '\n')
+                done = true;
+        }
     }
-  }
 }
 
 // this function is NOT reentrant!
-int messenger_recv(uint8_t *dst_buf) {
-  uint32_t dec_len;
-  int word_cnt, last_byte, dec_stat;
-  size_t bytes_read;
+int messenger_recv(uint8_t *dst_buf)
+{
+    uint32_t dec_len;
+    int word_cnt, last_byte, dec_stat;
 
-  // 2 bytes for CRLF
-  static uint8_t b64_buf[((MESSAGE_MAX * 4) / 3) + 2];
+    // 2 bytes for CRLF
+    static uint8_t b64_buf[((MESSAGE_MAX * 4) / 3) + 2];
 
-  // first byte of b64 decoded data indicates number of 4 byte chunks
-  // read 2 extra bytes for CRLF
-  UART2_read(uart, b64_buf, 6, &bytes_read);
+    // first byte of b64 decoded data indicates number of 4 byte chunks
+    // read 2 extra bytes for CRLF
+    UART2_read(uart, b64_buf, 6, NULL);
 
-  dec_len = base64_decode(dst_buf, b64_buf, 4, &dec_stat);
-  if (dec_stat < 0) {
-    _recv_crlf();
-    return -1;
-  }
+    dec_len = base64_decode(dst_buf, b64_buf, 4, &dec_stat);
+    if (dec_stat < 0)
+    {
+        _recv_crlf();
+        return -1;
+    }
 
-  word_cnt = dst_buf[0];
-  if (word_cnt > (MESSAGE_MAX >> 2)) {
-    // too big or some sync issue
-    _recv_crlf();
-    return -2;
-  }
+    word_cnt = dst_buf[0];
+    if (word_cnt > (MESSAGE_MAX >> 2))
+    {
+        // too big or some sync issue
+        _recv_crlf();
+        return -2;
+    }
 
-  if (word_cnt > 1) {
-    UART2_read(uart, b64_buf + 6, (word_cnt - 1) << 2, &bytes_read);
-  }
+    if (word_cnt > 1)
+    {
+        UART2_read(uart, b64_buf + 6, (word_cnt - 1) << 2, NULL);
+    }
 
-  // make sure CRLF terminator is present
-  last_byte = word_cnt << 2;
-  if (b64_buf[last_byte] != '\r' || b64_buf[last_byte + 1] != '\n') {
-    // malformed data/sync error
-    _recv_crlf();
-    return -3;
-  }
+    // make sure CRLF terminator is present
+    last_byte = word_cnt << 2;
+    if (b64_buf[last_byte] != '\r' || b64_buf[last_byte + 1] != '\n')
+    {
+        // malformed data/sync error
+        _recv_crlf();
+        return -3;
+    }
 
-  // convert to binary
-  dec_len = base64_decode(dst_buf, b64_buf, last_byte, &dec_stat);
-  if (dec_stat < 0) {
-    // malfored data/sync error
-    _recv_crlf();
-    return dec_stat - 10;
-  }
+    // convert to binary
+    dec_len = base64_decode(dst_buf, b64_buf, last_byte, &dec_stat);
+    if (dec_stat < 0)
+    {
+        // malfored data/sync error
+        _recv_crlf();
+        return dec_stat - 10;
+    }
 
-  // return length of received message
-  return dec_len;
+    // return length of received message
+    return dec_len;
 }
 
-// write a message to be sent out from the radio
-void messenger_send(const uint8_t *src_buf, unsigned src_len) {
-  uint32_t enc_len, bytes_remaining, total_bytes_sent;
-  size_t bytes_sent;
+void messenger_send(const uint8_t *src_buf, unsigned src_len)
+{
+    uint32_t enc_len, bytes_remaining, total_bytes_sent;
+    size_t bytes_sent;
 
-  // 2 bytes for CRLF
-  static uint8_t b64_buf[((MESSAGE_MAX * 4) / 3) + 2];
+    // 2 bytes for CRLF
+    static uint8_t b64_buf[((MESSAGE_MAX * 4) / 3) + 2];
 
-  enc_len = base64_encode(b64_buf, src_buf, src_len);
-  b64_buf[enc_len] = '\r';
-  b64_buf[enc_len + 1] = '\n';
+    enc_len = base64_encode(b64_buf, src_buf, src_len);
+    b64_buf[enc_len] = '\r';
+    b64_buf[enc_len + 1] = '\n';
 
-  bytes_remaining = enc_len + 2; // two byte CRLF
-  total_bytes_sent = 0;
-  while (bytes_remaining) {
-    // sometimes, even in blocking mode, UART_write returns before the
-    // complete buffer was sent, due to some queues being full
-    // update for the UART2 code, return code is not the bytes sent but rather
-    // the status code
-    int status = UART2_write(uart, b64_buf + total_bytes_sent, bytes_remaining,
-                             &bytes_sent);
-    if (status < 0)
-      return; // error, shouldn't happen
-    bytes_remaining -= bytes_sent;
-    total_bytes_sent += bytes_sent;
-  }
+    bytes_remaining = enc_len + 2; // two byte CRLF
+    total_bytes_sent = 0;
+    while (bytes_remaining)
+    {
+        // sometimes, even in blocking mode, UART_write returns before the
+        // complete buffer was sent, due to some queues being full
+        int status = UART2_write(uart, b64_buf + bytes_sent, bytes_remaining, &bytes_sent);
+        if (status < 0) return; // error, shouldn't happen
+        bytes_remaining -= bytes_sent;
+        total_bytes_sent += bytes_sent;
+    }
 }
